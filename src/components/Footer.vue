462c58285b217dbb0a1f669951c4689323dab945
孤梧 <dxs@duanxs.com>
2023-05-05 20:36:50 +0800

    feat: 新增备案信息

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,6 +6,7 @@
         style="color:inherit"
       >木兰开放作品许可协议 署名-相同方式共享</a>
       <span ml-2>2023 至今 © 孤梧</span>
+      <span><a href="https://beian.miit.gov.cn/" target="_blank">晋ICP备16005633号-5</a></span>
     </div>
   </footer>
 </template>
